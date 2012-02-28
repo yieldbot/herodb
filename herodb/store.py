edda014ba98d458f085f2f5c577f249b527ec1b5
Dave White <dwhite@yieldbot.com>
2012-02-28 17:47:23 -0500

    Convert keys to str so dulwich can handle them properly.

--- herodb/store.py
+++ herodb/store.py
@@ -223,7 +223,7 @@ class Store(object):
 def flatten(d, parent_key='', sep='/'):
     items = []
     for k, v in d.items():
-        new_key = parent_key + sep + k if parent_key else k
+        new_key = str(parent_key + sep + k) if parent_key else str(k)
         if isinstance(v, collections.MutableMapping):
             items.extend(flatten(v, new_key, sep).items())
         else:
