d580602c7bfa1255f7a49c0df236c7c8319789c8
Dave White <dwhite@yieldbot.com>
2012-04-09 12:31:39 -0400

    Fix s3backup working with bare repos.

--- herodb/s3backup.py
+++ herodb/s3backup.py
@@ -17,18 +17,18 @@ backup_stores_path = args[1]
 bucket = args[2]
 
 for store in os.listdir(stores_path):
-    if os.path.exists("%s/%s.git" % (stores_path, store)):
-        backup_store_path = "%s/%s.git" % (backup_stores_path, store)
+    if store.endswith('.git'):
+        backup_store_path = "%s/%s" % (backup_stores_path, store)
         if os.path.exists(backup_store_path):
-            cmd = "git fetch %s/%s.git" % (stores_path, store)
+            cmd = "git fetch %s/%s" % (stores_path, store)
             cwd = backup_store_path
         else:
-            cmd = "git clone %s/%s.git ." % (stores_path, store)
+            cmd = "git clone %s/%s ." % (stores_path, store)
             cwd = backup_stores_path
         # git clone/fetch to backup stores location
         subprocess.check_call(cmd, cwd=cwd, shell=True)
         # tarball of backup git repo
-        subprocess.check_call("tar -czf %s.tgz %s.git" % (store, store), cwd=backup_stores_path, shell=True)
+        subprocess.check_call("tar -czf %s.tgz %s" % (store, store), cwd=backup_stores_path, shell=True)
         # push backup git repo tarball to S3
         subprocess.check_call("s3put -b %s %s.tgz" % (bucket, store), cwd=backup_stores_path, shell=True)
         # remove the tarball locally
