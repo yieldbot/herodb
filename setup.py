ab1110fb07d07aff772bbf500eb54fd7ec7cc3bf
Dave White <dwhite@yieldbot.com>
2013-05-01 13:31:35 -0400

    small fix to not pass trailing slash when building path

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 setup(
     name = "herodb",
-    version = "0.2.2",
+    version = "0.2.3",
     packages = find_packages(exclude="test"),
 	install_requires = ['dulwich>=0.8.3', 'bottle>=0.10.9', 'requests>=1.1.0'],
 	setup_requires=['nose>=1.0'],
